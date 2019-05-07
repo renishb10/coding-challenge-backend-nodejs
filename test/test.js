65780e13ce8c8c21cba02747ad86286ab52e19bf
Renish B <renish.bhaskaran@globalenglish.com>
2019-05-07 20:41:27 +0530

    basic unit test setup

--- test/test.js
+++ test/test.js
@@ -1,23 +1,15 @@
 'use strict';
 
+// Dependencies
 const chai = require('chai');
 const expect = require('chai').expect;
-
 chai.use(require('chai-http'));
 
-const app = require('../src/server'); // Our app
+// Reference to main app
+const app = require('../src/server');
 
+// Cases Endpoint
 describe('API endpoint /cases', function() {
-  this.timeout(8000); // How long to wait for a response (ms)
-
-  before(function() {
-
-  });
-
-  after(function() {
-
-  });
-
   // GET - List all cases
   it('should return all Cases', function() {
     return chai.request(app)
@@ -25,18 +17,23 @@ describe('API endpoint /cases', function() {
       .then(function(res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-       // expect(res.body).to.be.an('object');
-        //expect(res.body.results).to.be.an('array');
+        expect(res.body).to.be.an('array');
+        expect(res.body[0]).to.have.property('id')
       });
   });
+});
 
+
+// Police Endpoint
+describe('API endpoint /polices', function() {
   // GET - List all Polices
   it('should return all Polices', function() {
     return chai.request(app)
       .get('/api/v1/polices')
-      .then(function(res) {
+      .then(function(res) {           
         expect(res).to.have.status(200);
         expect(res).to.be.json;
+        expect(res.body[0]).to.have.property('id')
        // expect(res.body).to.be.an('object');
         //expect(res.body.results).to.be.an('array');
       });
